f1e0578463e520a15abe3ffe3f919c6e0d8bf27e
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-18 13:02:13 +0530

    [ADDED]:Dipak - This Program Check The Less and Grether Than Value For Nested_ifelse

---
+++ Nested_ifelse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Condition_Statement
+{
+    class Nested_ifelse
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter The Two values:");
+            int a = Convert.ToInt16(System.Console.ReadLine());
+            int b = Convert.ToInt16(System.Console.ReadLine());
+            if (a == b)
+            {
+                System.Console.WriteLine("Values Is Equal");
+            }
+            else {
+                if (a > b)
+                {
+                    System.Console.WriteLine("A is Greater");
+                }
+                else {
+                    System.Console.WriteLine("A is Less Than");
+                }
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
