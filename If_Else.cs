73737b0b09cd1ef5572b69f541973e8b0b1cf11c
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-18 12:51:10 +0530

    [ADDED]:Dipak - This Program Check Even-odd Number For if-else

---
+++ If_Else.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Condition_Statement
+{
+    class If_Else
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Enter The Any Number:-");
+            int n = Convert.ToInt16(System.Console.ReadLine());
+            if (n % 2 == 0){
+                System.Console.WriteLine("Number Is Even");
+            }else {
+                System.Console.WriteLine("Number Is Odd");
+            }
+            System.Console.Read();
+        }
+    }
+}
