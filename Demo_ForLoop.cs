d4294292601299856fa64e5abcba1586e6dd640c
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-19 15:21:55 +0530

    [ADDED]:Dipak - Demonstrate This Program Simple For Loop in CSharp

---
+++ Demo_ForLoop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Loops_Statement
+{
+    class Demo_ForLoop
+    {
+        static void Main(string[] args)
+        {
+            int Count;
+            for (Count = 1; Count <=10; Count++) {
+                System.Console.WriteLine(Count);
+            }
+            System.Console.ReadLine();                                                                                                                                                        
+        }
+    }
+}
