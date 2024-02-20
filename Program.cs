8d8d796360ed8b59b043c89fbcc5afad1e1b53fc
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-20 19:41:23 +0530

    [ADDED]:Dipak - This Program Array And Many Function Use One Code

--- Program.cs
+++ Program.cs
@@ -3,20 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Condition_Statement
+namespace ArrayFunDemo
 {
     class Program
     {
+            int[] a = new int[5];
+            void input()
+            {
+               Console.WriteLine("Enter the 5 Elements:");
+                for (int i = 0; i < a.Length; i++)
+                {
+                 a[i] = Convert.ToInt16( Console.ReadLine() );
+                }
+            }
+            void output()
+            {
+                Console.WriteLine("The Array Elements Are:");
+                for (int i = 0; i < a.Length; i++)
+                {
+                 Console.WriteLine(a[i]);
+                }
+            }
+            void arrayOperations()
+            {
+                Console.WriteLine("Max No=" + a.Max());
+                Console.WriteLine("Min No=" + a.Min());
+                Console.WriteLine("Sum =" + a.Sum());
+                Console.WriteLine("Average=" + a.Average());
+            }
         static void Main(string[] args)
         {
-            System.Console.Write("Enter The Age:-");
-           int age =Convert.ToInt16(System.Console.ReadLine());
-            if(age>=18){
-                System.Console.WriteLine("Allowed For Voting");
-            }else{
-                System.Console.WriteLine("Not Allowed For Voting ");
-            }
-            System.Console.Read();
+            Program obj = new Program();
+
+            obj.input();
+            obj.output();
+            obj.arrayOperations();
+            Console.Read();
         }
     }
 }
