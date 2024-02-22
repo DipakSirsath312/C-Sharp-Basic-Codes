8fcec26ef5983f9fda7717c22060a3ec4956eef7
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-22 15:18:18 +0530

    [ADDED]:Dipak - This Program Change The Array Index

---
+++ ChangeArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AccessArray
+{
+    class ChangeArray
+    {
+        public static void Main(string[] args) {
+
+            int[] numbers = {1,2,3};
+            System.Console.WriteLine("Old value at Index 0:-"+numbers[1]);
+
+            numbers[1]= 11;
+            System.Console.WriteLine("New Value At Index 0:-"+numbers[1]);
+
+            Console.ReadLine();
+        }
+    }
+}
