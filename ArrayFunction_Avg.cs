b151c09145a3d530c2e7f5064aa5053175d6b046
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-22 20:58:01 +0530

    [ADDED]:Dipak - This Function _Average Number Provide

---
+++ ArrayFunction_Avg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AccessArray
+{
+    class ArrayFunction_Avg
+    {
+        public static void Main(string[] args) {
+            int[] numbers = { 9, 1, 5, 10, 7 };
+
+            float sum = numbers.Sum();
+            int count = numbers.Count();
+            float average = sum / count;
+
+            System.Console.WriteLine("Average:-"+average);
+            System.Console.WriteLine("Average Using Average():-"+numbers.Average());
+
+            System.Console.ReadLine();
+        }
+    }
+}
