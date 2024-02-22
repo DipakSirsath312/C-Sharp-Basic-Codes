e787aae481d9007dbbf901ef11187f971630b9e3
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-22 21:06:05 +0530

    [ADDED]:Dipak - Demonstrate Program Array_Foreach Loop

---
+++ Array_ForeachLoop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AccessArray
+{
+    class Array_ForeachLoop
+    {
+        public static void Main(string[] args) {
+            int[] numbers = {0, 1, 2, 4 };
+            System.Console.WriteLine("Array Elements:-");
+            foreach (int num in numbers) {
+                System.Console.WriteLine(num);
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
