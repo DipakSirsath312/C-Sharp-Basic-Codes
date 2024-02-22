05372a1786d41b8db73f4332d05e66a618f175a6
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-22 15:02:25 +0530

    [ADDED]:Dipak - This Program ForLoop Array Print The Value And Index

---
+++ Else_if.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Else_if{
+	public static void main(String args[]){
+			Scanner sc=new Scanner(System.in);
+			System.out.print("Enter The Button:-");
+			int Button=sc.nextInt();
+			if(Button==1){
+				System.out.println("Hello");
+				}else if(Button==2){
+					System.out.println("Namaste");
+					}else if(Button==3){
+						System.out.println("Ram Ram Bhaiyo");
+						}else{
+							System.out.println("Invailid Button");
+							}
+		}
+}
