cf6ad6a278d0aaa419869b7785b399d11d7c258f
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-22 14:25:37 +0530

    [ADDED]:Dipak - Demonstrate Program print Index Number

--- Program.cs
+++ Program.cs
@@ -3,42 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ArrayFunDemo
+namespace AccessArray
 {
     class Program
     {
-            int[] a = new int[5];
-            void input()
-            {
-               Console.WriteLine("Enter the 5 Elements:");
-                for (int i = 0; i < a.Length; i++)
-                {
-                 a[i] = Convert.ToInt16( Console.ReadLine() );
-                }
-            }
-            void output()
-            {
-                Console.WriteLine("The Array Elements Are:");
-                for (int i = 0; i < a.Length; i++)
-                {
-                 Console.WriteLine(a[i]);
-                }
-            }
-            void arrayOperations()
-            {
-                Console.WriteLine("Max No=" + a.Max());
-                Console.WriteLine("Min No=" + a.Min());
-                Console.WriteLine("Sum =" + a.Sum());
-                Console.WriteLine("Average=" + a.Average());
-            }
         static void Main(string[] args)
         {
-            Program obj = new Program();
+            int[]Numbers ={1,2,3};
 
-            obj.input();
-            obj.output();
-            obj.arrayOperations();
-            Console.Read();
+            System.Console.WriteLine("Element in First Index:-"+Numbers[0]);
+            System.Console.WriteLine("Element in First Index:-"+Numbers[1]);
+            System.Console.WriteLine("Element in First Index:-"+Numbers[2]);
+
+            System.Console.ReadLine();
         }
     }
 }
