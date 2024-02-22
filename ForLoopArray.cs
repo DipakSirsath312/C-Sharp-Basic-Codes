d31f73290ad1f2207ed9fc463b69f1e06ff61d7d
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-22 14:43:52 +0530

    [ADDED]:Dipak - This Program For Loop Array  Print The Element&Index

---
+++ ForLoopArray.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AccessArray
+{
+    class ForLoopArray
+    {
+        public static void Main(string[] args) {
+            int[] numbers = { 1, 2,4 };
+            for (int i = 0; i < numbers.Length; i++) {
+                System.Console.WriteLine("Element in index"+i+":"+numbers[i]);
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
