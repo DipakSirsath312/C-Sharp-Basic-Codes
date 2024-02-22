962d6be33cad7085108741d32927f9c2cd944f18
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-22 14:32:37 +0530

    [ADDED]:Dipak - This Program is For Finding Minimum & Maximum Number

---
+++ MiniMaxi_Array.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AccessArray
+{
+    class MiniMaxi_Array
+    {
+        public static void Main(string[] args) {
+            int[] numbers = { 51, 1, 3, 4, 98 };
+
+            System.Console.WriteLine("Largest Element:-"+numbers.Max());
+            System.Console.WriteLine("Smallest Element:-"+numbers.Min());
+
+            System.Console.ReadLine();
+        }
+    }
+}
