85249f6a60142dacc41e39e2feb20930c835ae18
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-24 13:46:33 +0530

    [ADDED]:Dipak - Demonstrate Program All Operation In One Code

--- Program.cs
+++ Program.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace AccessArray
+namespace Assignment1_CSharp.net
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[]Numbers ={1,2,3};
-
-            System.Console.WriteLine("Element in First Index:-"+Numbers[0]);
-            System.Console.WriteLine("Element in First Index:-"+Numbers[1]);
-            System.Console.WriteLine("Element in First Index:-"+Numbers[2]);
+            System.Console.WriteLine("***Operation in Single Code***");
+            int a, b,c;
+            System.Console.Write("Eneter The First Number:-");
+            a = Convert.ToInt16(System.Console.ReadLine());
+            System.Console.Write("Eneter The second Number:-");
+            b = Convert.ToInt16(System.Console.ReadLine());
+           
+            c = a + b;
+            System.Console.WriteLine("Addition:-"+c);
+            c = a - b;
+            System.Console.WriteLine("Subtraction:-"+c);
+            c = a * b;
+            System.Console.WriteLine("Multiplication:-"+c); 
+            c = a / b;
+            System.Console.WriteLine("Division:-"+c);
 
             System.Console.ReadLine();
         }
