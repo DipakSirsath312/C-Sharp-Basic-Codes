81f5687eb49df4273db0257e2e25b01371cdcf06
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-24 14:23:37 +0530

    [ADDED]:Dipak - This Program Check The Number Evevn And odd

---
+++ Even_or_Odd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment1_CSharp.net
+{
+    class Even_or_Odd
+    {
+        static void Main(string[] args) {
+
+            System.Console.WriteLine("***Check The Number Even And Odd***");
+            
+            System.Console.Write("Enter The Number:-");
+            int num = Convert.ToInt16(System.Console.ReadLine());
+            if(num%2==0){
+                System.Console.WriteLine("Number is Even");
+            }
+            else{
+                System.Console.WriteLine("Number is odd");
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
