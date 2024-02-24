fd7f7901105e06baf1a10a15bd0cff0bbe9bacfe
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-24 13:57:36 +0530

    [ADDED]:Dipak - This Program Print The Fibbonaci Seriese

---
+++ Fibbonacci_seriese.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment1_CSharp.net
+{
+    class Fibbonacci_seriese
+    {
+        public static void Main(string[] args) {
+            System.Console.WriteLine("***Print The Fibbonaci Series***");
+
+            int a = 0, b = 1, c = 0, n = 0, i;
+            System.Console.WriteLine("Enter A Number:-");
+            n = Convert.ToInt16(System.Console.ReadLine());
+
+            System.Console.WriteLine(a + "\n" + b);
+            for (i = 3; i <= n; i++)
+            {
+                c = a + b;
+                System.Console.Write("\n" + c);
+
+                a = b;
+                b = c;
+            }
+            System.Console.Read();
+
+        }
+    }
+}
