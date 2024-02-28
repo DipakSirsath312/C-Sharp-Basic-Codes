9e3f540fa3353a7ab09df42ee1a0bf98f0c0b2c9
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-28 21:17:18 +0530

    [ADDED]:Dipak - This Program Check User Age And Find The Allowed For Voting

---
+++ Condition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assigement
+{
+    class Condition
+    {
+        public static void Main(string[] args)
+        {
+            int age=0;
+            System.Console.WriteLine("Enter The Age:");
+            age = Convert.ToInt16(System.Console.ReadLine());
+            if (age >= 18)
+            {
+                System.Console.WriteLine("Allowed For Voting");
+            }
+            else 
+            {
+                System.Console.WriteLine("Not Allowed For Voting");
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
