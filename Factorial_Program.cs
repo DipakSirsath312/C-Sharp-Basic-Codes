4c83591da699ea7e2a76da621f05182768cc8244
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-28 21:00:15 +0530

    [ADDED]:Dipak - This Program Will be Find Factorial Number

---
+++ Factorial_Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assigement
+{
+    class Factorial_Program
+    {
+        public static void Main(string[] args) {
+            System.Console.WriteLine("***Find Factorial of Given Number***");
+
+            int num, fact = 1;
+            System.Console.Write("Enter The Any Number:");
+            num = Convert.ToInt16(System.Console.ReadLine());
+            while (num != 0) {
+                fact = fact * num;
+                num--;
+            }
+            System.Console.WriteLine("Factorial Number is:-"+fact);
+            System.Console.ReadLine();
+        }
+
+    }
+}
