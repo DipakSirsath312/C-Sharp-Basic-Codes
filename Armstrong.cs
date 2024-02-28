211a9c62cbc4b8a575b336aff2aa2642e68bddb4
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-28 21:22:25 +0530

    [ADDED]:Diapk - This Program Check Will Be Number Armstrong

---
+++ Armstrong.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assigement
+{
+    class Armstrong
+    {
+        public static void main(string[] args)
+        {
+            System.Console.WriteLine("***Check The Number Armstrong***");
+
+            int n = 0, j, sum = 0, temp = 0;
+            System.Console.Write("Enter The Number:-");
+            n = Convert.ToInt16(System.Console.ReadLine());
+            temp = n;
+            while (n > 0)
+            {
+                j = n % 10;
+                sum = sum + (j * j * j);
+                n = n / 10;
+
+            }
+            if (temp == sum)
+            {
+                System.Console.WriteLine("Number Is Armstrong");
+            }
+            else
+            {
+                System.Console.WriteLine("Number Is Not Armstrong");
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
