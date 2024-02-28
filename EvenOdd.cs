fd7d22fafea1aad2abd87a71742e17a8e6822dc5
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-28 20:51:22 +0530

    [ADDED]:Dipak - This Program Will Be Check The Even or Odd Number In User Input

---
+++ EvenOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assigement
+{
+    class EvenOdd
+    {
+        public static void Main(string[] args) {
+            System.Console.WriteLine("***Check The Even&Odd Number***");
+
+            int n = 0;
+            System.Console.Write("Enter a Any number:");
+            n = Convert.ToInt16(System.Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                System.Console.WriteLine("Number is Even:-" + n);
+            }
+            else {
+                System.Console.WriteLine("Number is Odd:-"+n);
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
