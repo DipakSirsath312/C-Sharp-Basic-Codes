f0e337acb462fcc62ff155d39ac1136fa4165b9d
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-28 20:32:45 +0530

    [ADDED]:Dipak - Demonstrate Program Two Values Input And Perform Addition

--- Program.cs
+++ Program.cs
@@ -3,29 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Assignment1_CSharp.net
+namespace Assigement
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("***Operation in Single Code***");
-            int a, b,c;
-            System.Console.Write("Eneter The First Number:-");
-            a = Convert.ToInt16(System.Console.ReadLine());
-            System.Console.Write("Eneter The second Number:-");
-            b = Convert.ToInt16(System.Console.ReadLine());
-           
-            c = a + b;
-            System.Console.WriteLine("Addition:-"+c);
-            c = a - b;
-            System.Console.WriteLine("Subtraction:-"+c);
-            c = a * b;
-            System.Console.WriteLine("Multiplication:-"+c); 
-            c = a / b;
-            System.Console.WriteLine("Division:-"+c);
+            System.Console.WriteLine("Enter The Values:");
+            int a = Convert.ToInt16(System.Console.ReadLine());
+            int b = Convert.ToInt16(System.Console.ReadLine());
+            int c = a + b;
+            System.Console.WriteLine("Addition="+c);
 
             System.Console.ReadLine();
         }
+        
     }
 }
